b988f8d39af99110e33ec56f60579ba197de76ed
Na <nguoivuive1986@gmail.com>
2023-04-15 12:57:12 +0700

    thaina

--- recommend_product.py
+++ recommend_product.py
@@ -13,13 +13,22 @@ import matplotlib.pyplot as plt
 from sklearn import metrics
 import seaborn as sns
 from gensim import corpora, models, similarities
+
 # 1. Read data
 data = pd.read_pickle('model/product_list.pkl')
+data = data.reset_index(drop=True)
+lst_user = [1,2,3,4,5,6,7,10,935,4429,3769]
+lst_products = data['product_name'].unique()
 dictionary = pd.read_pickle('model/Dictionary.pkl')
 index = pd.read_pickle('model/Index.pkl')
 
-cols=['user_id','recommendations']
-#df_new = pd.read_parquet('Recommend_User.parquet', columns=cols)
+cols=['user_idx','recommendations']
+df_new = pd.read_parquet('Recommend_User.parquet', columns=cols)
+
+cols_idx = ['product_id','product_idx']
+df_idx = pd.read_parquet('data_index.parquet', columns=cols_idx)
+
+
 
 # TFIDF
 tfidf = pickle.load(open("model/Tfidf.pkl", "rb")) 
@@ -27,16 +36,20 @@ tfidf = pickle.load(open("model/Tfidf.pkl", "rb"))
 def path_to_image_html(path):
     return '<img src="' + path + '" width="60" >'
 
-# def get_recommendations_list_idx(user_id):
-#     list_product_id = []
-#     for i in range(5):
-#         idx = df_new[df_new['user_id']==user_id]['recommendations'].index.values[0]
-#         list_product_id.append(df_new[df_new['user_id_idx']==user_id]['recommendations'][idx][i]['product_id'])
-#     return list_product_id
+def get_recommendations_list_idx(user_id):
+    list_product_id = []
+    for i in range(10):
+        idx = df_new[df_new['user_idx']==user_id]['recommendations'].index.values[0]
+        list_product_id.append(df_new[df_new['user_idx']==user_id]['recommendations'][idx][i]['product_idx'])
+    return list_product_id
+
+def get_product_info(id):
+    product_name= data[data['product_id']==id][['product_id','product_name','link','image','price']]
+    return product_name
 
 #Create function to get top 10 relatest products
 def top10_relatest_search(text,dictionary):
-    data1 = data[["product_id","product_name","price","rating","image","link"]]
+    data1 = data[["product_id","product_name","price","rating","image"]]
     text = text.lower().split()
     kw_vector = dictionary.doc2bow(text)
     sim = index[tfidf[kw_vector]]
@@ -45,33 +58,52 @@ def top10_relatest_search(text,dictionary):
     sortedDict = sorted(dictionary.items(),key=lambda x:x[1])
     last_key = list(sortedDict)[-10:-1]
     for i in range (len(last_key)):
-        lst = np.append(last_key[i][0],lst)
-    #     # get image and title of top 10 similar products_id
-    #     lst_link = data[data['product_id'].isin(sim(keyword))]['image'].tolist()
+        lst = np.append(last_key[i][0],lst)      
+    df_final =  data1.iloc[lst, :].reset_index(drop=True)
+    df_final.drop_duplicates(inplace=True)
+    return df_final
+
+def show_image(df):
+    lst = df['product_id'].tolist()
+    for i in range(10):
+        # get image and title of top 10 similar products_id
+        lst_link = df[df['product_id'].isin(lst)]['image'].tolist()
         
-    #     # get index of element in lst_link != N/A
-    #     lst_index = [i for i, x in enumerate(lst_link) if str(x) != 'nan']
-    #     # drop N/A value in lst_link
-    #     lst_link = [x for x in lst_link if str(x) != 'nan']
+        # get index of element in lst_link != N/A
+        lst_index = [i for i, x in enumerate(lst_link) if str(x) != 'nan']
+        # drop N/A value in lst_link
+        lst_link = [x for x in lst_link if str(x) != 'nan']
         
-    #     # get list title of lst_link = product_name     
-    #     lst_title = data[data['product_id'].isin(sim(keyword))]['product_name'].tolist()
-    #     # get title of lst_link = product_name from list lst_title with index in lst_index
-    #     lst_title = [lst_title[i] for i in lst_index]
+        # get list title of lst_link = product_name     
+        lst_title = df[df['product_id'].isin(lst)]['product_name'].tolist()
+        # get title of lst_link = product_name from list lst_title with index in lst_index
+        lst_title = [lst_title[i] for i in lst_index]
         
-    #     # get list product_id of lst_link = product_id
-    #     lst_product_id = data[data['product_id'].isin(sim(keyword))]['product_id'].tolist()
-    #     # get product_id of lst_link = product_id from list lst_product_id with index in lst_index
-    #     lst_product_id = [str(lst_product_id[i]) for i in lst_index]
+        # get list product_id of lst_link = product_id
+        lst_product_id = df[df['product_id'].isin(lst)]['product_id'].tolist()
+        # get product_id of lst_link = product_id from list lst_product_id with index in lst_index
+        lst_product_id = [str(lst_product_id[i]) for i in lst_index]
         
-    #     # concat lst_title and lst_product_id
-    #     lst_title = [lst_title[i] + ' - ' + lst_product_id[i] for i in range(len(lst_title))]
+        # concat lst_title and lst_product_id
+        lst_title = [lst_title[i] + ' - ' + lst_product_id[i] for i in range(len(lst_title))]
         
-    #     st.image(lst_link, 
-    #                 width=150, 
-    #                 caption=lst_title,
-    #     )
-    return data1.iloc[lst, :].reset_index(drop=True)
+    st.image(lst_link, 
+                width=150, 
+                caption=lst_title,
+    )
+
+    #return df_final
+
+def show_by_user(id):
+    product_lst = pd.unique(df_idx[df_idx['product_idx'].isin(get_recommendations_list_idx(id))]['product_id'])
+    data_show=data[data['product_id'].isin(product_lst)].drop_duplicates().reset_index(drop=True)[['product_id','product_name','image','price']]
+    st.dataframe(data=data_show)
+    show_image(data_show)
+
+def show_by_product_name(name):
+    df_by_name = top10_relatest_search(name,dictionary)
+    st.dataframe(data=df_by_name)
+    show_image(df_by_name)
 
 
     
@@ -106,16 +138,18 @@ if choice == '1.Business Objective':
 
 
 elif choice == '2.Recommendation':
-    st.markdown("**Input some keywords related products that you want to search...**")
-    option = st.radio("select type",options = ("Recommend products by keyword","Recommend by user"))
-    if option == "Recommend products by keyword":
+    option = st.radio("Select type",options = ("Content based Filtering by input keyword","Collaborative Filtering from available product name","Collaborative Filtering"))
+    if option == "Content based Filtering by input keyword":
         keyword = st.text_area("Input your keyword: ")
         if keyword != "":
             st.write("Some recommended products are:...")
-            st.markdown(top10_relatest_search(keyword,dictionary).to_html(render_links=True, escape=False),unsafe_allow_html=True)    
-    if option == "Recommend by user":
-        #st.dataframe(df_new)
-        keyword2 = st.text_area("Input user_id: ")
-        # if keyword2 != "":
-        #     st.write("Some recommended products are:...")
-        #     st.markdown(get_recommendations_list_idx(user_id=keyword2))
+            st.dataframe(data=top10_relatest_search(keyword,dictionary),use_container_width=False)
+            show_image(top10_relatest_search(keyword,dictionary))
+            #st.markdown(top10_relatest_search(keyword,dictionary).to_html(render_links=True, escape=False),unsafe_allow_html=True)    
+    if option == "Collaborative Filtering from available product name":
+        option2=st.selectbox("Select name of product:",options=lst_products)
+        show_by_product_name(option2)
+    if option == "Collaborative Filtering":
+        option3=st.selectbox("Select user_id:",options=lst_user)
+        show_by_user(option3)
+        
